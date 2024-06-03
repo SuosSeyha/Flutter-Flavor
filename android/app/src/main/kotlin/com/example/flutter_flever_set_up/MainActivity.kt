d4b08544145fd30242b88ae7b9d4f7139dd4dc91
PhannCheat <engexoeng@gmail.com>
2024-06-03 11:09:19 +0700

    flutter-flavor

---
+++ android/app/src/main/kotlin/com/example/flutter_flever_set_up/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flever_set_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flever_set_up/global.dart';
+import 'package:flutter_flever_set_up/my_app.dart';
+
+void main() {
+  Global.baseUrl = 'https//todo-aws/prod/';
+  runApp(const MyApp());
+}
